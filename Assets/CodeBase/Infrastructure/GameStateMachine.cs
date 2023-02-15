b758835acefc7b5b0946799ae18df406728943af
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-02-15 14:43:47 +0300

    Add game state machine

---
+++ Assets/CodeBase/Infrastructure/GameStateMachine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeBase.Infrastructure
+{
+    public class GameStateMachine
+    {
+        private IState _activeState;
+        private readonly Dictionary<Type, IState> _states;
+
+        public GameStateMachine()
+        {
+            _states = new Dictionary<Type, IState>();
+        }
+        
+        public void Enter<TState>() where TState : IState
+        {
+            _activeState.Exit();
+            
+            IState state = _states[typeof(TState)];
+            _activeState = state;
+            
+            state.Enter();
+        }
+    }
+}
