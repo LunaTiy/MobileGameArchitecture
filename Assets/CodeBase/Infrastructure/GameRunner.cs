82a023948577c362f57c24b41bfd24c9b18c0841
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-02-21 22:18:36 +0300

    Add game runner from any scene

---
+++ Assets/CodeBase/Infrastructure/GameRunner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+    public class GameRunner : MonoBehaviour
+    {
+        [SerializeField] private GameBootstrapper _bootstrapperPrefab;
+
+        private void Awake()
+        {
+            var bootstrapper = FindObjectOfType<GameBootstrapper>();
+
+            if (bootstrapper == null)
+                Instantiate(_bootstrapperPrefab);
+        }
+    }
+}
