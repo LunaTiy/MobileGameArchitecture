7b6643d4a61146aad42e98504772319e3a76bf50
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-02-28 18:42:22 +0300

    Add HeroDeath script

---
+++ Assets/CodeBase/Hero/HeroDeath.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Hero
+{
+    [RequireComponent(typeof(HeroHealth))]
+    public class HeroDeath : MonoBehaviour
+    {
+        [SerializeField] private HeroHealth _health;
+        [SerializeField] private HeroMover _heroMover;
+        [SerializeField] private HeroAnimator _heroAnimator;
+        [SerializeField] private GameObject _deathFx;
+        
+        private bool _isDead;
+
+        private void OnEnable() => 
+            _health.healthChanged += HealthChangedHandler;
+
+        private void OnDisable() => 
+            _health.healthChanged -= HealthChangedHandler;
+
+        private void HealthChangedHandler()
+        {
+            if (CanDie())
+                Die();
+        }
+
+        private bool CanDie() => 
+            !_isDead && _health.CurrentHp <= 0;
+
+        private void Die()
+        {
+            _isDead = true;
+            _heroMover.enabled = false;
+            _heroAnimator.PlayDeath();
+
+            Instantiate(_deathFx, transform.position, Quaternion.identity);
+        }
+    }
+}
