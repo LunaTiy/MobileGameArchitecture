d18427e194a5230ed86ffe8121a3c5cfd3f9eab6
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-03-09 20:35:26 +0300

    Add SO for monsters

---
+++ Assets/CodeBase/StaticData/MonsterStaticData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace CodeBase.StaticData
+{
+    [CreateAssetMenu(fileName = "MonsterData", menuName = "StaticData/Monster")]
+    public class MonsterStaticData : ScriptableObject
+    {
+        public MonsterTypeId monsterTypeId;
+        
+        [Range(1f, 100f)] public float maxHp;
+        
+        [Range(1f, 30f)] public float damage;
+        [Range(0.5f, 3f)] public float attackCooldown;
+        [Range(0.5f, 2f)] public float attackRange;
+
+        public GameObject prefab;
+    }
+}
